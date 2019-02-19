19d5f94a68fdd744df9d64bbda973ae950659ce8
KonradWrbl <160815@stud.prz.edu.pl>
2019-02-19 21:05:19 +0100

    now you can check the water bu you cant delete your search :(

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,70 @@
 const API = '//api.openweathermap.org/data/2.5/';
-
+let maxTemp;
+let minTem;
 
 $('.searchButton').on('click', () => {
-    axios.get(`${API}weather?q=${$("#searchInput").value}&appid=e86ba166de2e36b28f351cc82f422e7f`)
-        .then((response) => { console.log(response) })
-        .catch((error) => { console.log(error) })
+
+    if ($("#searchInput")[0].value === '') {
+        alert('Podaj nazwe miasta!');
+        return;
+    }
+
+    axios.get(`${API}weather?q=${$("#searchInput")[0].value}&appid=e86ba166de2e36b28f351cc82f422e7f`)
+        .then((response) => {
+            console.log(response);
+            maxTemp = Math.round(response.data.main.temp_max - 273.3);
+            cloudy = response.data.weather[0].main;
+            city = response.data.name
+            addResult();
+
+         })
+        .catch((error) => { console.log(error) });
+
+    $("#searchInput")[0].value = '';
+});
+
+let addResult = () => {
+
+    let resultItem = document.createElement('div'),
+        cityEl = document.createElement('div'),
+            cityNameEl = document.createElement('p'),
+        tempEl = document.createElement('div'),
+            temperatureEl = document.createElement('p'),
+        cloudEl = document.createElement('img'),
+        closeEl = document.createElement('div'),
+            closeBtn = document.createElement('button');
+
+    cityNameEl.innerText = `${city}`;
+    temperatureEl.innerText = `${maxTemp} ℃`;
+    if (cloudy === 'Clear') {
+        cloudEl.setAttribute('src', './img/sunny.svg');
+    } else if (cloudy === 'Clouds') {
+        cloudEl.setAttribute('src', './img/clouds.svg');
+    } else if (cloudy === 'Rain') {
+        cloudEl.setAttribute('src', './img/raining.svg');
+    }
+
+    resultItem.classList.add('resultItem');
+    cityEl.classList.add('city');
+    tempEl.classList.add('temp');
+    cloudEl.classList.add('cloud');
+    closeEl.classList.add('close');
+    closeBtn.classList.add('closeBtn')
+
+    closeEl.appendChild(crossBtn);
+    closeBtn.innerHTML = '<img src="./img/delete.svg" alt="">';
+
+    cityEl.appendChild(cityNameEl);
+    tempEl.appendChild(temperatureEl);
+
+    resultItem.appendChild(cityEl);
+    resultItem.appendChild(tempEl);
+    resultItem.appendChild(cloudEl);
+    resultItem.appendChild(closeEl);
+
+    $('.results')[0].append(resultItem);
+}
+
+$('.closeBtn').on('click', () => {
+    $('.resultItem').remove();
 });
