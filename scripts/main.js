7cad77b5f1299bcbc06572120531e4a0256b29d8
KonradWrbl <160815@stud.prz.edu.pl>
2019-02-19 17:02:20 +0100

    some tests

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
-import axios from 'axios';
+const API = '//api.openweathermap.org/data/2.5/';
 
-const API = 'api.openweathermap.org/data/2.5/';
 
 $('.searchButton').on('click', () => {
     axios.get(`${API}weather?q=${$("#searchInput").value}&appid=e86ba166de2e36b28f351cc82f422e7f`)
